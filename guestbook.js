8d0bcf1882a82b2eb70a060072964b1fc34522fc
Vadim-M <93524278+VadimMalkin@users.noreply.github.com>
2022-08-09 17:55:58 +0200

    Create guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://ead7231c.eu-de.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
